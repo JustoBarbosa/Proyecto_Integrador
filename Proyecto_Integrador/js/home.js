fa42a78cb59877b1dde883b20e73080ca5b95196
JustoBarbosa <jbarbosamoreira@udesa.edu.ar>
2024-06-23 22:40:36 -0300

    reset del js del home (arranca noche epica)

--- Proyecto_Integrador/js/home.js
+++ Proyecto_Integrador/js/home.js
@@ -1,42 +1,5 @@
-document.addEventListener("DOMContentLoaded",() =>{
-    const apiURL = "https://fakestoreapi.com/products" ;
-
-    const fetchProducts = async () => {
-        try {
-            const response = await fetch(apiURL);
-            const data = await response.json();
-            renderProducts(data);  
-        }
-        catch (error){
-            console.error(Error)
-        }
-    };
-    fetchProducts();
-});
-
-//secciones del dom
-const seccionelectronica = document.querySelector(".Electronica");
-const seccionAccesorios = document.querySelector(".Accesorios");
-const seccionHombre = document.querySelector(".Hombre");
-const seccionMujer = document.querySelector(".Mujer");
-
-const renderProducts = (products) => {
-    products.forEach(products => {
-        const productElement = createProductElement(product);
-
-        switch (product.category) {
-            case "Electronica":
-                seccionelectronica.appendChild(productElement);
-                break;
-            case "Accesorios":
-                seccionAccesorios.appendChild(productElement);
-                break;
-            case "Hombre":
-                seccionHombre.appendChild(productElement);
-                break;
-            case "Mujer":
-                seccionMujer.appendChild(productElement);
-                break;
-        }
-    });
+window.onload = function(){
+    fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=>console.log(json))
 }
