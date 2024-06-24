4415175de2bcdae7625036b69a626635794863ff
JustoBarbosa <jbarbosamoreira@udesa.edu.ar>
2024-06-24 00:13:35 -0300

    terminada mi seccion del home esperando la de marcos para proseguir con categorias y detalles del producto

--- Proyecto_Integrador/js/home.js
+++ Proyecto_Integrador/js/home.js
@@ -1,9 +1,36 @@
 window.onload = function(){
     fetch('https://fakestoreapi.com/products')
     .then(function(response){
-        return response.json()
+        return response.json();
     })
-    .then(function(informacion){
-        console.log(informacion);
+    .then(function(products){
+        
+        let accesorios = products.filter(product => product.category === 'jewelery');
+        let ropaHombre = products.filter(product => product.category === "men's clothing");
+        let ropaMujer = products.filter(product => product.category === "women's clothing");
+
+        let accesorioscontainer = document.getElementById('accesorios')
+        accesorioscontainer.innerHTML = ""
+        if(accesorioscontainer){
+            accesorios.forEach(product =>{
+                let interior =`
+                    <ul>
+                        <li>
+                            <h2>${product.title}</h2>
+                            <img src="${product.image}" alt="${product.title}">
+                            <li>${product.description}</li>
+                            <p>${product.price}$</p>
+                            <a href="detalle_producto.html?id=${product.id}">Ver detalles del producto</a>
+                        </li>
+                    </ul>
+                `;
+                accesorioscontainer.innerHTML += interior
+                console.log(accesorioscontainer.innerHTML);
+            ;
+        })}
+    
     })
+    .catch(function(error){
+        console.log('Error: ' + error);
+    });
 }
